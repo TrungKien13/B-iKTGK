079888b15d257bee8b598d2827c4d230aee995b8
trinhvu204 <trinhvu0303@gmail.com>
2024-10-24 15:56:46 +0700

    aaa

--- VoTrungKien_2280601607/KTGK/KTGK/De01/frmSinhvien.Designer.cs
+++ VoTrungKien_2280601607/KTGK/KTGK/De01/frmSinhvien.Designer.cs
@@ -52,6 +52,7 @@
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSV)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -185,6 +186,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.dtNgaySinh);
             this.groupBox1.Controls.Add(this.txtHoTen);
             this.groupBox1.Controls.Add(this.txtMaSV);
@@ -271,6 +273,15 @@
             this.groupBox2.TabIndex = 10;
             this.groupBox2.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(662, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Vuxinchao";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmSinhvien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -319,6 +330,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DateTimePicker dtNgaySinh;
+        private System.Windows.Forms.Button button1;
     }
 }
 
